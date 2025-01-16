ab2e2e6b642e992e7a51c2b6e70838ca1d60e0e0
tawoe <tawoe@posteo.de>
2025-01-16 11:00:13 +0100

    make app root URI configurable

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ sio=socketio.AsyncServer(cors_allowed_origins='*',async_mode='asgi')
 
 #wrap with ASGI application
 socket_app = socketio.ASGIApp(sio)
-app.mount("/", socket_app)
+mount_path = os.getenv('MOUNT_PATH', '/opey')
+app.mount(mount_path, socket_app)
 
 # configure logging
 logging.basicConfig(level=logging.INFO)
